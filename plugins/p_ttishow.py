484b2f90603dc6133ed2803beeabe5bcf47372a5
I <96487144+k-n-o-x@users.noreply.github.com>
2022-12-27 21:54:27 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -22,7 +22,7 @@ async def save_group(bot, message):
         if message.chat.id in temp.BANNED_CHATS:
             # Inspired from a boat of a banana tree
             buttons = [[
-                InlineKeyboardButton('🌐 Support', url=f'https://t.me/greymatters_bots_discussion')
+                InlineKeyboardButton('💠 BackUp 💠', url=f'https://t.me/HeyPikachunub')
             ]]
             reply_markup=InlineKeyboardMarkup(buttons)
             k = await message.reply(

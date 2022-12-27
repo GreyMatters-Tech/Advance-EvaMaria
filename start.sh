c285f1d5c9ad8dbfb02029a37c2ee8ac0c31dffe
I <96487144+k-n-o-x@users.noreply.github.com>
2022-12-27 23:08:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Greymattersbot/EvaMaria /EvaMaria
+  git clone https://github.com/k-n-o-x/Advance-EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria

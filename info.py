01d4f0e9f0334f541c9b8903f910e6a5f9dc605e
I <96487144+k-n-o-x@users.noreply.github.com>
2022-12-27 23:04:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,10 +74,10 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://t.me/download_from_search_zone_bot"
+DOWNLOAD_TEXT_URL = "https://t.me/Pikachunubadmin_bot"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Subscribe"
-CAPTION_BUTTON_URL = "https://youtube.com/@GreyMattersYT"
+CAPTION_BUTTON_URL = "https://t.me/Pikachunub"
 
    # Auto Delete For Bot Sending Files #

5ea2a6c5394134bc457e225cb541a9a3f468fdc8
I <96487144+k-n-o-x@users.noreply.github.com>
2022-12-27 21:45:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('🤖 𝚄𝚙𝚍𝚊𝚝𝚎𝚜', url='https://t.me/Pikachunub')
+                InlineKeyboardButton('📣 𝚄𝚙𝚍𝚊𝚝𝚎𝚜 📣', url='https://t.me/Pikachunub')
             ],
             [
                 InlineKeyboardButton('❗𝙷𝚎𝚕𝚙❗', url=f"https://t.me/{temp.U_NAME}?start=help"),
@@ -45,7 +45,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ 𝙰𝚍𝚍 𝙼𝚎 𝚃𝚘 𝚈𝚘𝚞𝚛 𝙶𝚛𝚘𝚞𝚙𝚜 ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 𝚂𝚎𝚊𝚛𝚌𝚑', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 𝚄𝚙𝚍𝚊𝚝𝚎𝚜', url='https://t.me/Pikachunub')
+            InlineKeyboardButton('📣 𝚄𝚙𝚍𝚊𝚝𝚎𝚜 📣', url='https://t.me/Pikachunub')
             ],[
             InlineKeyboardButton('❗𝙷𝚎𝚕𝚙❗', callback_data='help'),
             InlineKeyboardButton('😊 𝙰𝚋𝚘𝚞𝚝', callback_data='about')
@@ -91,7 +91,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ 𝙰𝚍𝚍 𝙼𝚎 𝚃𝚘 𝚈𝚘𝚞𝚛 𝙶𝚛𝚘𝚞𝚙𝚜 ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 𝚂𝚎𝚊𝚛𝚌𝚑', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 𝚄𝚙𝚍𝚊𝚝𝚎𝚜', url='https://t.me/Pikachunub')
+            InlineKeyboardButton('📣 𝚄𝚙𝚍𝚊𝚝𝚎𝚜 📣', url='https://t.me/Pikachunub')
             ],[
             InlineKeyboardButton('❗𝙷𝚎𝚕𝚙❗', callback_data='help'),
             InlineKeyboardButton('😊 𝙰𝚋𝚘𝚞𝚝', callback_data='about')
